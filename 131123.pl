a61892fe05f8b2196a5d63cab4cb324a5b8d7641
João Vitor de Oliveira Mendes <68301483+JoaoVitorOliMendes@users.noreply.github.com>
2023-11-13 22:10:49 -0300

    Update 131123.pl

--- 131123.pl
+++ 131123.pl
@@ -21,3 +21,19 @@ substantivo(s,f) --> [gata] | [rata].
 substantivo(p,m) --> [gatos] | [ratos].
 substantivo(p,f) --> [gatas] | [ratas].
 verbo --> [caçou].
+
+frase --> sujeito(N), predicado(N) | sujeito_composto(N), predicado(p).
+sujeito(N) --> artigo(N,G), substantivo(N,G).
+predicado(N) --> verbo(N), artigo(M,G), substantivo(M,G).
+artigo(s,m) --> [o] | [um].
+artigo(s,f) --> [a] | [uma].
+artigo(p,m) --> [os] | [uns].
+artigo(p,f) --> [as] | [umas].
+substantivo(s,m) --> [gato] | [rato].
+substantivo(s,f) --> [gata] | [rata].
+substantivo(p,m) --> [gatos] | [ratos].
+substantivo(p,f) --> [gatas] | [ratas].
+verbo(s) --> [caçou].
+verbo(p) --> [caçaram].
+sujeito_composto(N) --> sujeito(N), conjuncao_soma, sujeito(N).
+conjuncao_soma --> [e].
