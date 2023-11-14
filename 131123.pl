20a86d39ef123a2b9eaccba38c931928d0f795ff
João Vitor de Oliveira Mendes <68301483+JoaoVitorOliMendes@users.noreply.github.com>
2023-11-13 21:47:32 -0300

    Update 131123.pl

--- 131123.pl
+++ 131123.pl
@@ -1,3 +1,4 @@
+/*
 frase --> sujeito, predicado.
 sujeito --> artigo(G), substantivo(G).
 predicado --> verbo, artigo(G), substantivo(G).
@@ -5,4 +6,18 @@ artigo(m) --> [o] | [um].
 artigo(f) --> [a] | [uma].
 substantivo(m) --> [gato] | [rato].
 substantivo(f) --> [gata] | [rata].
+verbo --> [caçou].
+*/
+
+frase --> sujeito, predicado.
+sujeito --> artigo(N,G), substantivo(N,G).
+predicado --> verbo, artigo(N,G), substantivo(N,G).
+artigo(s,m) --> [o] | [um].
+artigo(s,f) --> [a] | [uma].
+artigo(p,m) --> [os] | [ums].
+artigo(p,f) --> [as] | [umas].
+substantivo(s,m) --> [gato] | [rato].
+substantivo(s,f) --> [gata] | [rata].
+substantivo(p,m) --> [gatos] | [ratos].
+substantivo(p,f) --> [gatas] | [ratas].
 verbo --> [caçou].
