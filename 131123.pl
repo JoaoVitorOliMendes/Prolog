8dbb9ceac2dcd8d21a6e4fcddd62d0056a2c37fa
João Vitor de Oliveira Mendes <68301483+JoaoVitorOliMendes@users.noreply.github.com>
2023-11-13 21:40:50 -0300

    Create 131123.pl

---
+++ 131123.pl
@@ -0,0 +1,8 @@
+frase --> sujeito, predicado.
+sujeito --> artigo(G), substantivo(G).
+predicado --> verbo, artigo(G), substantivo(G).
+artigo(m) --> [o] | [um].
+artigo(f) --> [a] | [uma].
+substantivo(m) --> [gato] | [rato].
+substantivo(f) --> [gata] | [rata].
+verbo --> [caçou].
